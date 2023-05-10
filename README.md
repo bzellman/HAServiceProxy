0497d8a6444fd6c91a031e38edb636d77deea9fc
Bradley Zellman <bszells@gmail.com>
2023-05-10 18:24:31 -0500

    change approach again

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # HAServiceProxy
 
-A custom Home Assistant add-on to securely access internal services.
+This Home Assistant add-on allows you to access internal services through Home Assistant, either by embedding them in iframes on your Lovelace dashboard or as a sidebar item.
 
 ## Installation
 
-1. Add this repository URL in Home Assistant: `https://github.com/bzellman/HAServiceProxy`
-2. Install the "ServiceProxy" add-on from the "Your Add-ons" section in the Add-on Store.
-3. Configure the add-on by entering the required information, such as internal IP addresses, service names, and SSL certificate paths.
-4. Start the add-on.
+1. In Home Assistant, navigate to Supervisor > Add-on Store.
+2. Click the three-dot menu in the top right corner, and select Repositories.
+3. Add the repository URL for this add-on and click "Add".
+4. Find "HAServiceProxy" in the list of available add-ons and click on it.
+5. Click "Install".
 
-## Usage
+## Configuration
 
-You can access the internal services via iframes in a Lovelace dashboard or as an add-on item in the Home Assistant sidebar.
+The add-on uses a YAML configuration. You can add your services by specifying their IP addresses, names, and whether they should be shown in the sidebar.
+
+Example:
--- haserviceproxy/run.sh
+++ haserviceproxy/run.sh
@@ -1,9 +1,47 @@
 #!/bin/sh
 
-# Read options from the configuration file
-. /services.sh
-SERVICES="$(jq --raw-output '.services | @base64' /data/options.json)"
-SIDEBAR_SERVICES="$(jq --raw-output '.sidebar_services | @base64' /data/options.json)"
-SSL_CERT="$(jq --raw-output '.ssl_cert' /data/options.json)"
-SSL_KEY="$(jq --raw-output '.ssl_key' /data/options.json)"
-LOG_LEVEL="$(jq --raw-output '.
+# Get the SSL key and cert paths from the options
+ssl_key_path="$(jq -r '.ssl_key_path' /data/options.json)"
+ssl_cert_path="$(jq -r '.ssl_cert_path' /data/options.json)"
+
+# Generate nginx configuration file
+cat << EOF > /etc/nginx/conf.d/default.conf
+server {
+  listen 80;
+
+  location / {
+    root /usr/share/nginx/html;
+    index index.html;
+  }
+
+  location /api/ {
+    proxy_pass http://192.168.0.206:443;
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    auth_request /auth;
+  }
+
+  location = /auth {
+    internal;
+    proxy_pass http://192.168.0.206:443/api/hassio_auth;
+    proxy_pass_request_body off;
+    proxy_set_header Content-Length "";
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+  }
+
+  # Add locations for each internal service
+  $(for service in "${!services[@]}"; do
+    echo "location /${services[$service]["name"]}/ {";
+    echo "  proxy_pass http://${services[$service]["ip_address"]};";
+    echo "  auth_request /auth;";
+    echo "}";
+  done)
+}
+EOF
+
+# Start nginx
+nginx -g 'daemon off;'
